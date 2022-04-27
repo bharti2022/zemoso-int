25915f831de3da1b304104150dee95f48c48bdef
bharti2022 <bharti.sagar@zemosolabs.com>
2022-04-28 01:02:31 +0530

    test case added

---
+++ src/test/java/com/example/springboot/springassignment/controller/LoginControllerTests.java
@@ -0,0 +1,22 @@
+package com.example.springboot.springassignment.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginControllerTests {
+    LoginController loginController =new LoginController();
+    @Test
+    public void loginPage(){
+        assertEquals("user-login",loginController.showMyLoginPage());
+    }
+    @Test
+    public void showAccessDeniedPage(){
+        assertEquals("access-denied",loginController.showAccessDenied());
+    }
+
+}
